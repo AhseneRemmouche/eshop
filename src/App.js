317fe094c198aee9c2da3d07a0b54994d0dab42c
AhseneRemmouche <ahsene.remmouche@outlook.com>
2024-10-20 17:30:54 -0400

    commit header paart 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import StorefrontIcon from '@mui/icons-material/Storefront';
 
 function App() {
   return (
     <div className="App">
+      <h1>Bismi Allah</h1>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         <StorefrontIcon /> 
-         <StorefrontIcon />
-          Learn React
-        </a>
+       <div className="header_logo"></div>
+       <div className="header_search"></div>
+       <div className="header_nav">
+        <div className="nav_item">
+          <div className="nav_itemLineOne">Salam Guest</div>
+          <div className="nav_itemLineTwo">Sign In</div>
+        </div>
+        <div className="nav_item">
+        <div className="nav_itemLineOne">Your</div>
+        <div className="nav_itemLineTwo">Shop</div>
+        </div>
+        <div className="nav_item">
+        <div className="nav_itemLineOne">Basket</div>
+        <div className="nav_itemLineTwo">0</div>
+        </div>
+       </div>
+  
       </header>
     </div>
   );
